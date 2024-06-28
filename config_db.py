7d0499f74bfa30782d276030ecca6bccb72b6033
Oh-deam <prshndmtr@yandex.ru>
2024-06-28 06:26:04 +0200

    modified burse_parser.py

--- config_db.py
+++ config_db.py
@@ -4,11 +4,11 @@ import psycopg2
 
 host = "localhost"
 
-try:        # Пробуем подключаться к БД
-    connection = psycopg2.connect(
-        host=host
-    )
-    pass
-except Exception as _ex:
-    print("[INFO] Error while working with PostgresSQL", _ex)
-finally:        # Будем закрывать соединение
+# try:        # Пробуем подключаться к БД
+#     connection = psycopg2.connect(
+#         host=host
+#     )
+#     pass
+# except Exception as _ex:
+#     print("[INFO] Error while working with PostgresSQL", _ex)
+# finally:        # Будем закрывать соединение
